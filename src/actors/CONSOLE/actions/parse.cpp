e4d183bc151b869ac6a330973b13293801c279ab
Igor Goryachev <igor@tegia.ru>
2024-07-14 23:44:33 +0300

    Запуск lambda как актора

--- src/actors/CONSOLE/actions/parse.cpp
+++ src/actors/CONSOLE/actions/parse.cpp
@@ -4,6 +4,9 @@
 
 #include "../CONSOLE.h"
 
+#include <tegia/types/types.h>
+
+
 ////////////////////////////////////////////////////////////////////////////////////////////
 /**
 
@@ -16,6 +19,48 @@
 
 namespace EXAMPLE {
 
+
+auto parse_lambda = [](const std::shared_ptr<message_t> &message) -> int
+{
+	std::cout << _YELLOW_ << "RUN TEST" << _BASE_TEXT_ << std::endl;
+
+	tegia::types::phone_t phone;
+	int res = phone.parse(message->data["input"].get<std::string>());
+
+	switch(res)
+	{
+		case 0:
+		{
+			message->data["phone"] = "";
+			message->data["is_valid"] = false;
+			message->data["code"] = phone.code();
+			message->data["thread"] = tegia::context::tid();
+		}
+		break;
+
+		case 1:
+		{
+			message->data["phone"] = phone.value();
+			message->data["is_valid"] = true;
+			message->data["code"] = phone.code();
+			message->data["thread"] = tegia::context::tid();
+		}
+		break;
+
+		case 2:
+		{
+			message->data["phone"] = "";
+			message->data["is_valid"] = false;
+			message->data["code"] = phone.code();
+			message->data["thread"] = tegia::context::tid();
+		}
+		break;
+	}
+
+	return 200;
+};
+
+
 int CONSOLE::parse(const std::shared_ptr<message_t> &message)
 {
 	/////////////////////////////////////////////////////////////////////////////////////////  
@@ -23,10 +68,13 @@ int CONSOLE::parse(const std::shared_ptr<message_t> &message)
 	auto send_message_lambda = [](std::string_view token)
 	{
 		if(token == "")	return;
+
 		auto msg = tegia::message::init();
 		msg->data["input"] = token;
 		msg->callback.add("example/console","/out");
-		tegia::message::send("example/phone","/parse",msg);
+		tegia::message::send(msg,parse_lambda);
+
+		// std::cout << msg->data << std::endl;
 	};
 
 	std::string input = message->data["input"].get<std::string>();
